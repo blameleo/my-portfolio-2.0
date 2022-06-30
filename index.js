932b9c432bd90c4f67d401fb3dea536082461a4d
Leonard Adjei <82063118+blameleo@users.noreply.github.com>
2022-06-30 07:49:49 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const toggle = document.getElementById('nav-toggle')
+const navLinks = document.querySelectorAll('.navlist')
+
+toggle.addEventListener('click', () =>{
+    document.body.classList.toggle('nav-open');
+
+})
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
